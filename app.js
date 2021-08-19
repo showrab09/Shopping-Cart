3b17abd163fdf440d2c9fcb508261049b19d2b88
showrab09 <showrabbinhabib@gmail.com>
2021-08-20 03:08:05 +0600

    Logic Complete

---
+++ app.js
@@ -0,0 +1,46 @@
+function updateProductNumber(product,price,increasing) {
+    const productInput = document.getElementById(product+"-number");
+    let productNumber = productInput.value;
+    if (increasing == true) {
+        productNumber = parseInt(productNumber)+1;
+    } else if (productNumber > 0) {
+      productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    const productTotal = document.getElementById(product+'-total');
+    productTotal.innerText = productNumber * price;
+}
+function subTotal() {
+    const phoneTotal = document.getElementById('phone-total');
+    phoneTotalPrice = parseInt(phoneTotal.innerText);
+    const caseTotal = document.getElementById('case-total');
+    caseTotalPrice = parseInt(caseTotal.innerText);
+    const subTotal = caseTotalPrice + phoneTotalPrice;
+    let subTotalprice = document.getElementById('sub-total');
+    subTotalprice.innerText = subTotal;
+    //tax
+    let taxAmount = document.getElementById("tax-amount");
+    taxAmount.innerText = subTotal * .10;
+    //totalprice
+    let totalPrice = document.getElementById("total-price");
+    totalPrice.innerText = subTotal + parseFloat(taxAmount.innerText);
+}
+
+//phone
+document.getElementById("phone-plus").addEventListener("click", function () {
+    updateProductNumber('phone', 1200, true);
+    subTotal();
+});
+document.getElementById("phone-minus").addEventListener("click", function () {
+    updateProductNumber("phone", 1200, false);
+    subTotal();
+});
+//case
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber("case", 60, true);
+    subTotal();
+});
+document.getElementById("case-minus").addEventListener("click", function () {
+    updateProductNumber("case", 60, false);
+    subTotal();
+});
